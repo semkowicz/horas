bc3fb765c5fd66427a5e1cbd7b10bd3e7f08ad16
Daniel Semkowicz <daniel@semkowicz.pl>
2024-12-30 19:08:09 +0100

    main: Parse command line arguments
    Get psalm number from a program argument.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use clap::Parser;
+
 mod divinum_officium;
 
+/// Tool for creating Liturgia Horarum booklets
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Psalm number
+    #[clap(required = true)]
+    psalm: u32,
+}
+
 fn main() -> Result<(), String> {
+    let args = Args::parse();
+
     let translations = divinum_officium::DivinumOfficium::new();
 
-    for line in translations.psalm(1)? {
+    for line in translations.psalm(args.psalm)? {
         println!("{line}");
     }
 
