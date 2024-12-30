dfecd355dac4c6a9170413d16d5fa69fa7395fce
Daniel Semkowicz <daniel@semkowicz.pl>
2024-12-30 19:08:09 +0100

    main: Parse command line arguments
    Define simple CLI interface using clap library.
    Read psalm number and tone from command line arguments and print
    them to the standard output.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+/// Tool for creating Liturgia Horarum booklets
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Psalm number
+    #[clap(required = true)]
+    psalm: u32,
+
+    /// Tone
+    #[clap(required = true)]
+    tone: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Psalm: {}, tone: {}", args.psalm, args.tone);
 }
